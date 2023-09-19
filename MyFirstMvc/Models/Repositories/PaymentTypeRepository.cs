20a85b5f673f8f0e5c57ca66116ec0618a2b534e
Randy <weton0110@gmail.com>
2023-09-20 00:26:56 +0800

    feat 清除購物車,建立訂單流程

---
+++ MyFirstMvc/Models/Repositories/PaymentTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using MyFirstMvc.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstMvc.Models.Repositories
+{
+    public class PaymentTypeRepository : BaseRepository<PaymentType>
+    {
+        private AppDbContext _db = new AppDbContext();
+
+        public PaymentType GetTypeName(int typeCode)
+        {
+            return _db.PaymentTypes.FirstOrDefault(x => x.Id == typeCode && x.Enable);
+        }
+    }
+}
