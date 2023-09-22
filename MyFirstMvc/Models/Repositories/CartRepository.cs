bb3f47fa60de89eebf79d880178c8e6cc6e4ca41
Eloise <drabbit0606@gmail.com>
2023-09-23 00:31:00 +0800

    修正跑版

--- MyFirstMvc/Models/Repositories/CartRepository.cs
+++ MyFirstMvc/Models/Repositories/CartRepository.cs
@@ -8,38 +8,38 @@ using System.Web;
 
 namespace MyFirstMvc.Models.Repositories
 {
-    public class CartRepository : BaseRepository<Cart>
-    {
-        private AppDbContext _db = new AppDbContext();
+	public class CartRepository : BaseRepository<Cart>
+	{
+		private AppDbContext _db = new AppDbContext();
 
-        /// <summary>
-        /// 透過會員Key取得購物車資訊
-        /// </summary>
-        /// <param name="memberAccount">會員key</param>
-        /// <returns></returns>
-        public Cart GetByMember(string account)
-        {
-            return _db.Carts.FirstOrDefault(x => x.MemberAccount == account);
-        }
+		/// <summary>
+		/// 透過會員Key取得購物車資訊
+		/// </summary>
+		/// <param name="account"></param>
+		/// <returns></returns>
+		public Cart GetByMember(string account)
+		{
+			return _db.Carts.FirstOrDefault(x => x.MemberAccount == account);
+		}
 
-        public int AddNew(Cart cart)
-        {
-            _db.Carts.Add(cart);
-            _db.SaveChanges();
-            return cart.Id;
-        }
+		public int AddNew(Cart cart)
+		{
+			_db.Carts.Add(cart);
+			_db.SaveChanges();
+			return cart.Id;
+		}
 
-        /// <summary>
-        /// 清空指定 User 購物車
-        /// </summary>
-        /// <param name="account"></param>
-        public void EmptyCart(string account)
-        {
-            var cart = _db.Carts.FirstOrDefault(c => c.MemberAccount == account);
-            if (cart == null) return;
+		/// <summary>
+		/// 清空指定 User 購物車
+		/// </summary>
+		/// <param name="account"></param>
+		public void EmptyCart(string account)
+		{
+			var cart = _db.Carts.FirstOrDefault(c => c.MemberAccount == account);
+			if (cart == null) return;
 
-            _db.Carts.Remove(cart);
-            _db.SaveChanges();
-        }
-    }
+			_db.Carts.Remove(cart);
+			_db.SaveChanges();
+		}
+	}
 }
